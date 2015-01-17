c1d484ad6b9b12ae715895154a4137d62d953773
plum <crazyplum24@gmail.com>
2015-01-17 23:13:06 +0800

    add files

---
+++ ER_model.py
@@ -0,0 +1,10 @@
+import networkx as nx
+from util import plot_dd
+
+
+
+
+if __name__ == '__main__':
+	# get parameter
+	# generate degree distribution graph with log-log scale
+	# count centrality
---
+++ util.py
@@ -0,0 +1,8 @@
+import networkx as nx
+
+def plot_dd(deg_seq, filename):
+	
+	'''input: list, output filename
+	   output: generate degree distribution graph with log-log scale
+	'''
+	
